2958688b0727772db25299b4de9a7863cca4b1e9
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-20 15:21:43 -0700

    tests passing

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useBlogs } from '../../hooks/useBlogs';
 import BlogCard from '../BlogCard/BlogCard';
 
-
 export default function Main() {
   const blogs = useBlogs();
   return (
